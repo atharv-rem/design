dc4e556547f86176148c7e67d04ad056ce472301
Atharv Remeshan <atharv.rem@gmail.com>
2025-08-31 15:53:42 +0530

    improved all the styles

--- design-index/app/components/grid.jsx
+++ design-index/app/components/grid.jsx
@@ -22,6 +22,7 @@ export default function Grid({items}) {
       (item) => item.category === "colour"
     );
   }
+
   return (
     <>
     {filtered.length > 0 ? (
@@ -31,30 +32,30 @@ export default function Grid({items}) {
             <Link
             key={item.tool_name}
             to={`/tool/${item.tool_name.toLowerCase()}`}
-            className="bg-white rounded-[15px] lg:rounded-[20px] xl:rounded-[24px] justify-start items-center border-[1px] border-[#e1e1e1] flex flex-row hover:shadow-sm hover:translate-y-[-2px] transition-all duration-200 ease-in-out cursor-pointer p-2"
+            className="bg-white rounded-[15px] lg:rounded-[20px] xl:rounded-[24px] justify-start items-center border-[1px] border-[#e1e1e1] flex flex-row hover:shadow-sm hover:translate-y-[-2px] transition-all duration-200 ease-in-out cursor-pointer"
             >
             <img
             alt={item.description.toLowerCase()}
             loading='lazy'
             src={item.og_image_link}
-            className="h-[110px] w-[190px] sm:h-[110px] sm:w-[200px] md:h-[130px] md:w-[240px] lg:h-[140px] lg:w-[240px] xl:w-[300px] xl:h-[160px] 2xl:w-[350px] 2xl:h-[195px] rounded-[10px] sm:rounded-[10px] md:rounded-[14px] lg:rounded-[14px] xl:rounded-[18px] flex-shrink-0 overflow-hidden mr-[12px]"
+            className="h-[110px] w-[190px] sm:h-[110px] sm:w-[200px] md:h-[130px] md:w-[240px] lg:h-[140px] lg:w-[240px] xl:w-[300px] xl:h-[160px] 2xl:w-[350px] 2xl:h-[195px] rounded-[10px] sm:rounded-[10px] md:rounded-[14px] lg:rounded-[14px] xl:rounded-[18px] flex-shrink-0 overflow-hidden ml-[7px] md:ml-[7px] lg:ml-[8px] xl:ml-[8px] 2xl:ml-[12px] mt-[5px] mb-[5px]"
             />
-            <div className='flex flex-col justify-start items-start w-full'>
-                <div className="text-[14px] lg:text-[17px] xl:text-[19px] 2xl:text-[25px] text-black font-Outfit font-bold">{item.tool_name}</div>
+            <div className='flex flex-col justify-start items-start ml-[15px] mr-[12px] w-auto h-full mt-[10px]'>
+                <div className="text-[14px] lg:text-[17px] xl:text-[19px] 2xl:text-[25px] text-black font-Outfit font-bold p-0 leading-4 xl:leading-5 mt-[5px] lg:mt-[8px] 2xl:mt-[15px]">{item.tool_name}</div>
 
-                {item.pricing.length > 5 ? (
-                    <div className='flex gap-2 mt-2'>
-                        <div className="font-Poppins font-semibold text-[11px] lg:text-[15px] xl:text-[17px] 2xl:text-[21px] py-[5px] px-[5px] text-[#20851c] bg-[#d9ffd7] rounded-[8px]">free</div>
-                        <div className="font-Poppins font-semibold text-[11px] lg:text-[15px] xl:text-[17px] 2xl:text-[21px] py-[5px] px-[5px] text-[#ab2a2a] bg-[#ffe5e5] rounded-[8px]">paid</div>
+                    {item.pricing.length > 5 ? (
+                    <div className='flex flex-row items-center justify-start'>
+                      <div className="font-Poppins font-semibold flex text-[11px] lg:text-[15px] xl:text-[17px] 2xl:text-[21px] py-[5px] px-[5px] text-[#20851c] bg-[#d9ffd7] w-[40px] h-[20px] lg:w-[45px] lg:h-[25px] xl:w-[50px] xl:h-[30px] 2xl:w-[60px] 2xl:h-[30px] justify-center items-center rounded-[8px] mt-[5px] lg:mt-[7px] 2xl:mt-[15px]">free</div>
+                      <div className="flex font-Poppins font-semibold text-[11px] lg:text-[15px] xl:text-[17px] 2xl:text-[21px] pt-[5px] pb-[5px] text-[#ab2a2a] bg-[#ffe5e5] w-[40px] h-[20px] lg:w-[45px] lg:h-[25px] xl:w-[50px] xl:h-[30px] 2xl:w-[60px] 2xl:h-[30px] justify-center items-center rounded-[8px] mt-[5px] lg:mt-[7px] ml-[5px] 2xl:mt-[15px]">paid</div>
                     </div>
                     ) : item.pricing === 'free' ? (
-                        <div className="font-Poppins font-semibold text-[11px] lg:text-[15px] xl:text-[17px] 2xl:text-[21px] py-[5px] px-[5px] text-[#20851c] bg-[#d9ffd7] rounded-[8px] mt-2">free</div>
+                        <div className="font-Poppins font-semibold flex text-[11px] lg:text-[15px] xl:text-[17px] 2xl:text-[21px] py-[5px] px-[5px] text-[#20851c] bg-[#d9ffd7] w-[40px] h-[20px] lg:w-[45px] lg:h-[25px] xl:w-[50px] xl:h-[30px] 2xl:w-[60px] 2xl:h-[30px] justify-center items-center rounded-[8px] mt-[5px] lg:mt-[7px] 2xl:mt-[15px]">free</div>
                     ) : item.pricing === 'paid' ? (
-                        <div className="font-Poppins font-semibold text-[11px] lg:text-[15px] xl:text-[17px] 2xl:text-[21px] py-[5px] px-[5px] text-[#ab2a2a] bg-[#ffe5e5] rounded-[8px] mt-2">paid</div>
+                        <div className="font-Poppins font-semibold text-[11px] lg:text-[15px] xl:text-[17px] 2xl:text-[21px] py-[5px] px-[5px] text-[#ab2a2a] bg-[#ffe5e5] w-[40px] h-[20px] lg:w-[45px] lg:h-[25px] xl:w-[50px] xl:h-[30px] 2xl:w-[60px] 2xl:h-[30px] justify-center items-center rounded-[8px] mt-[5px] lg:mt-[7px] 2xl:mt-[15px]">paid</div>
                     ) : null}
 
-                    <p className="text-[12px] lg:text-[15px] xl:text-[17px] 2xl:text-[20px] font-Outfit font-semibold text-[#595959] mt-2">
-                        {item.description.toLowerCase()}
+                    <p className="text-[12px] lg:text-[15px] xl:text-[17px] 2xl:text-[20px] font-Outfit font-semibold text-[#595959]  leading-[12px] xs:leading-[15px] sm:leading-3 md:leading-4 lg:leading-5 justify-evenly mr-[5px] mt-[5px] lg:mt-[7px] 2xl:mt-[10px]">
+                      {item.description.toLowerCase()}
                     </p>
             </div>
             </Link>
